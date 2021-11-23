8751fcf26ce9412eb469f44cf95b019ab052e7d5
Dennis Kroes <developer@dkroes.nl>
2021-11-23 11:45:36 +0100

    Add configuration (appsettings.json) loading to GUI project

--- WineCellar/WineCellar.GUI/App.xaml.cs
+++ WineCellar/WineCellar.GUI/App.xaml.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿using Controller;
+using Controller.Repositories;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +18,21 @@ namespace WineCellar
     /// </summary>
     public partial class App : Application
     {
+        public IConfiguration Configuration { get; private set; }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // Builder is used for configuring the way configuration is retrieved
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true);
+
+            // This creates the IConfiguration object
+            Configuration = builder.Build();
+
+            // DataAccess requires the configuration to create SqlConnections
+            DataAccess.SetConfiguration(Configuration);
+        }
     }
 }
