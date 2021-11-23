ea284f065ddcf57042031dc06b23f2f9a11f6795
Dennis Kroes <developer@dkroes.nl>
2021-11-23 16:21:49 +0100

    Central place for storing queries

---
+++ WineCellar/WineCellar.Controller/Queries.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller;
+
+internal static class Queries
+{
+    #region Stored procedures for table Wine
+    public const string Wine_Get =      "spWine_Get";
+    public const string Wine_GetAll =   "spWine_GetAll";
+    public const string Wine_Insert =   "spWine_Insert";
+    public const string Wine_Delete =   "spWine_Delete";
+    public const string Wine_Update =   "spWine_Update";
+    #endregion
+
+
+}
