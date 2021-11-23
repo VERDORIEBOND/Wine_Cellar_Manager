0a435af868069b0c6f15c66d942f7b50423519c6
Dennis Kroes <developer@dkroes.nl>
2021-11-23 16:22:00 +0100

    WineRepository initial draft

--- WineCellar/WineCellar.Controller/Repositories/WineRepository.cs
+++ WineCellar/WineCellar.Controller/Repositories/WineRepository.cs
@@ -1,21 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Dapper;
+using Model;
 
 namespace Controller.Repositories;
-
-public record WineRecord(int Id, string Name, decimal Buy, decimal Sell, int Type, int Country, string Picture, int Year, int Content, decimal Alcohol, int Rating, string Description);
 
 public class WineRepository
 {
     public async Task<WineRecord> Get(int id)
     {
-        using (var conn = DataAccess.GetConnection)
-        {
-            return await conn.QuerySingleAsync<WineRecord>("SELECT [Id],[Name],[Buy],[Sell],[Type],[Country],[Picture],[Year],[Content],[Alcohol],[Rating],[Description] FROM [dbo].[Wine] WHERE Id=@Id", new { Id = id });
-        }
+        using var conn = DataAccess.GetConnection;
+        return await conn.QuerySingleAsync<WineRecord>(Queries.Wine_Get, new { Id = id }, commandType: CommandType.StoredProcedure);
+    }
+
+    public async Task<IEnumerable<WineRecord>> GetAll()
+    {
+        using var conn = DataAccess.GetConnection;
+        return await conn.QueryAsync<WineRecord>(Queries.Wine_GetAll, commandType: CommandType.StoredProcedure);
+    }
+
+    public async Task<int> Delete(int id)
+    {
+        using var conn = DataAccess.GetConnection;
+        return await conn.ExecuteAsync(Queries.Wine_Delete, new { Id = id }, commandType: CommandType.StoredProcedure);
+    }
+
+    public async Task<int> Update(WineRecord wine)
+    {
+        using var conn = DataAccess.GetConnection;
+        return await conn.ExecuteAsync(Queries.Wine_Update, wine, commandType: CommandType.StoredProcedure);
+    }
+
+    public async Task<int> Create(WineRecord wine)
+    {
+        using var conn = DataAccess.GetConnection;
+        return await conn.ExecuteScalarAsync<int>(Queries.Wine_Insert, wine, commandType: CommandType.StoredProcedure);
     }
 }
---
+++ WineCellar/WineCellar.Model/DatabaseRecords.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model;
+
+public record WineRecord(int Id, string Name, decimal Buy, decimal Sell, int Type, int Country, string Picture, int Year, int Content, decimal Alcohol, int Rating, string Description);
