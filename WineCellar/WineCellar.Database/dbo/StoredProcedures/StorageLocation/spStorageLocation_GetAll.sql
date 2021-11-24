eb46c18d2933bb8d760535b981baf2afd1e20ea8
MonkeyOS <developer@dkroes.nl>
2021-11-24 10:26:40 +0100

    Added StoredProcedures for StorageLocation

---
+++ WineCellar/WineCellar.Database/dbo/StoredProcedures/StorageLocation/spStorageLocation_GetAll.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[spStorageLocation_GetAll]
+AS
+BEGIN
+	SELECT [IdWine], [Shelf], [Row], [Col]
+	FROM [dbo].[StorageLocation];
+END
---
+++ WineCellar/WineCellar.Database/dbo/StoredProcedures/StorageLocation/spStorageLocation_GetByWine.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[spStorageLocation_GetByWine]
+	@IdWine int
+AS
+BEGIN
+	SELECT [IdWine], [Shelf], [Row], [Col]
+	FROM [dbo].[StorageLocation]
+	WHERE [IdWine] = @IdWine;
+END
