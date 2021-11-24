bd0f7e38c27177de29991298a6d9c9c7d65f68fc
MonkeyOS <developer@dkroes.nl>
2021-11-24 09:44:50 +0100

    WineRepository update & delete tests

--- WineCellar/WineCellar.ControllerTest/Model_WineRepository_Should.cs
+++ WineCellar/WineCellar.ControllerTest/Model_WineRepository_Should.cs
@@ -15,6 +15,8 @@ namespace WineCellar.ControllerTest
     public class Model_WineRepository_Should
     {
         private IConfiguration? Configuration { get; set; }
+        private WineRecord Expected { get; set; } = new(0, "TestWine", 12.10m, 13.20m, 1, 1, "empty", 2001, 700, 12.50m, 3, "Beschrijving");
+        private int InsertedId { get; set; }
 
         [SetUp]
         public void Setup()
@@ -32,18 +34,50 @@ namespace WineCellar.ControllerTest
             DataAccess.SetConfiguration(Configuration);
         }
 
-        [Test]
+        [Test, Order(1)]
         public async Task WineRepository_CreateShould()
         {
-            WineRecord expected = new(0, "TestWine", 12.10m, 13.20m, 1, 1, "empty", 2001, 700, 12.50m, 3, "Beschrijving");
+            // Saving/creating the row in the database, also store Id locally for other tests
+            InsertedId = await DataAccess.WineRepo.Create(Expected);
+            Assert.IsTrue(InsertedId > 0, "Expected Create to return an integer larger than 0.");
 
-            int id = await DataAccess.WineRepo.Create(expected);
-            Assert.IsTrue(id > 0, "Expected Create to return an integer larger than 0.");
+            // Expected started without an id, so adding it here
+            Expected = Expected with { Id = InsertedId };
 
-            expected = expected with { Id = id };
-            WineRecord result = await DataAccess.WineRepo.Get(id);
+            // Checking if the result is the same as what was expected, records allow for easy comparisons
+            WineRecord result = await DataAccess.WineRepo.Get(InsertedId);
             Assert.IsNotNull(result, "WineRecord retrieved from the database is null.");
-            Assert.AreEqual(expected, result);
+            Assert.AreEqual(Expected, result);
+        }
+
+        [Test, Order(2)]
+        public async Task WineRepository_UpdateShould()
+        {
+            Assert.IsTrue(InsertedId > 0, "Expected InsertedId to return an integer larger than 0.");
+
+            // Change a few variables
+            Expected = Expected with { Name = "AlteredName!", Buy = 12.01m, Rating = 5 };
+
+            // Send the update to the database and check if rows affected is 1
+            int rowsAffected = await DataAccess.WineRepo.Update(Expected);
+            Assert.AreEqual(1, rowsAffected, $"Expected affected rows to be 1, it was {rowsAffected}");
+
+            // Verify the results in database
+            WineRecord result = await DataAccess.WineRepo.Get(InsertedId);
+            Assert.AreEqual(Expected, result, "Retrieved record does not match updated record");
+        }
+
+        [Test, Order(3)]
+        public async Task WineRepository_DeleteShould()
+        {
+            Assert.IsTrue(InsertedId > 0, "Expected InsertedId to return an integer larger than 0.");
+
+            // Send the deletion to the database and check if rows affected is 1
+            int rowsAffected = await DataAccess.WineRepo.Delete(InsertedId);
+            Assert.AreEqual(1, rowsAffected, $"Expected affected rows to be 1, it was {rowsAffected}");
+
+            // Verify that the row cannot be retrieved
+            Assert.ThrowsAsync<InvalidOperationException>(() => DataAccess.WineRepo.Get(InsertedId));
         }
     }
 }
