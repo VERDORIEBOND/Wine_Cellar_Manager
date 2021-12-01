1e392dd464fa41447696a9e7188e13a38ccad04a
Dennis Kroes <developer@dkroes.nl>
2021-12-01 11:09:31 +0100

    New database selection windows setup

---
+++ WineCellar/WineCellar.GUI/Views/DatabaseSetup/DatabaseSelectWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Controller;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WineCellar.Views.DatabaseSetup
+{
+    /// <summary>
+    /// Interaction logic for DatabaseSelectWindow.xaml
+    /// </summary>
+    public partial class DatabaseSelectWindow : Window
+    {
+        private DatabaseNewWindow _DatabaseNewWindow { get; set; }
+
+        public DatabaseSelectWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonNewConnection_Click(object sender, RoutedEventArgs e)
+        {
+            _DatabaseNewWindow = new DatabaseNewWindow();
+            _DatabaseNewWindow.ShowDialog();
+        }
+
+        private async void ButtonConnect_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
