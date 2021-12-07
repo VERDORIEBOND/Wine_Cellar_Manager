1ee7cfbba88c20160d98aa06b591adf1b4009ce5
Dennis Kroes <developer@dkroes.nl>
2021-12-07 11:45:09 +0100

    Add Database context related

---
+++ WineCellar/WineCellar.GUI/DataContexts/DatabaseNewContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WineCellar.DataContexts;
+
+public class DatabaseNewContext
+{
+    public string InputHost { get; set; }
+    public string InputPort { get; set; } = "1433";
+    public string InputUsername { get; set; }
+    public string InputPassword { get; set; }
+}
