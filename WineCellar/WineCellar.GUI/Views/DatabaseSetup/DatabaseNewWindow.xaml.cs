c15221b11e715fc767c984dc51cef15b2f77cd19
Dennis Kroes <developer@dkroes.nl>
2021-12-07 11:45:09 +0100

    Add Database context related

--- WineCellar/WineCellar.GUI/Views/DatabaseSetup/DatabaseNewWindow.xaml.cs
+++ WineCellar/WineCellar.GUI/Views/DatabaseSetup/DatabaseNewWindow.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Controller;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,6 +15,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WineCellar.DataContexts;
 
 namespace WineCellar.Views.DatabaseSetup
 {
@@ -19,9 +24,65 @@ namespace WineCellar.Views.DatabaseSetup
     /// </summary>
     public partial class DatabaseNewWindow : Window
     {
+        private static readonly Regex _numberRegex = new("[^0-9.-]+"); // Only allow numeric values
+
+        private DatabaseNewContext _DatabaseNewContext { get; set; }
+
         public DatabaseNewWindow()
         {
             InitializeComponent();
+
+            _DatabaseNewContext = new DatabaseNewContext();
+
+            DataContext = _DatabaseNewContext;
+        }
+
+        private bool ValidateFields()
+        {
+            if (_DatabaseNewContext.InputHost.Length == 0 ||
+                _DatabaseNewContext.InputPort.Length == 0 ||
+                _DatabaseNewContext.InputUsername.Length == 0 ||
+                _DatabaseNewContext.InputPassword.Length == 0)
+                return false;
+
+            return true;
+        }
+
+        private async void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!ValidateFields())
+                return;
+
+            progressConnection.Visibility = Visibility.Visible;
+
+            SqlConnectionStringBuilder builder = new();
+            builder.DataSource = $"{_DatabaseNewContext.InputHost},{_DatabaseNewContext.InputPort}";
+            builder.UserID = _DatabaseNewContext.InputUsername;
+            builder.Password = _DatabaseNewContext.InputPassword;
+            builder.Encrypt = false;
+            builder.ConnectTimeout = 10;
+            
+            try
+            {
+                await DataAccess.CheckConnectionFor(builder.ConnectionString);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                Debug.WriteLine(ex.StackTrace);
+            }
+
+            progressConnection.Visibility = Visibility.Hidden;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PortTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = _numberRegex.IsMatch(e.Text);
         }
     }
 }
