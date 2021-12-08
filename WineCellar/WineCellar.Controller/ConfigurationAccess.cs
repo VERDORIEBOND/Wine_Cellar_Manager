ee99fcfc414d02063a50f7cfbec2b7c2acdde65b
Dennis Kroes <developer@dkroes.nl>
2021-12-08 12:03:20 +0100

    Implemented newtonsoft.json for reading and creating database entries in appsettings

--- WineCellar/WineCellar.Controller/ConfigurationAccess.cs
+++ WineCellar/WineCellar.Controller/ConfigurationAccess.cs
@@ -1,7 +1,10 @@
-﻿using Model;
+﻿using Microsoft.Extensions.Configuration;
+using Model;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,29 +22,49 @@ public static class ConfigurationAccess
         // Load the appsettings.json as a JObject
         JObject appsettings = JObject.Parse(json);
         // Entry "Databases" is a keyvalue structure
-        JToken? databases = appsettings["Databases"];
+        JArray? databases = (JArray?)appsettings["Databases"];
 
         // Add the object for databases into the json if it doesn't exist
         if (databases is null)
         {
             appsettings["Databases"] = new JObject();
-            databases = appsettings["Databases"];
+            databases = (JArray?)appsettings["Databases"];
         }
 
         // Check if it exists and add an object to it
-        if (databases is not null && databases[information.Name] is not null)
+        if (databases is not null)
         {
             JObject databaseObj = new();
+            databaseObj["Name"] = information.Name;
             databaseObj["Host"] = information.Host;
             databaseObj["Port"] = information.Port;
             databaseObj["User"] = information.User;
             databaseObj["Password"] = information.Password;
             databaseObj["Database"] = information.Database;
 
-            databases[information.Name] = databaseObj;
+            databases.Add(databaseObj);
         }
 
         await File.WriteAllTextAsync(GetAppsettingsPath(), appsettings.ToString(Newtonsoft.Json.Formatting.Indented));
+    }
+
+    public static async Task<List<DatabaseInformation>> GetDatabasesAsync()
+    {
+        // Read the appsettings.json or appsettings.Development.json
+        string json = await File.ReadAllTextAsync(GetAppsettingsPath());
+        JObject appsettings = JObject.Parse(json);
+
+        // List for all found databases
+        List<DatabaseInformation> databases = new();
+
+        JArray? jsonDatabases = (JArray?)appsettings["Databases"];
+        if (jsonDatabases is not null)
+        {
+            // Turn the JArray into a list
+            databases = jsonDatabases.ToObject<List<DatabaseInformation>>() ?? databases;
+        }
+
+        return databases;
     }
 
     private static string GetAppsettingsPath()
--- WineCellar/WineCellar.GUI/DataContexts/DatabaseSelectContext.cs
+++ WineCellar/WineCellar.GUI/DataContexts/DatabaseSelectContext.cs
@@ -1,14 +1,33 @@
 ﻿using Model;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WineCellar.DataContexts;
 
-public class DatabaseSelectContext
+public class DatabaseSelectContext : INotifyPropertyChanged
 {
-    public List<DatabaseInformation> Databases { get; set; }
-    public DatabaseInformation SelectedDatabase { get; set; }
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private List<DatabaseInformation> _Databases;
+    public List<DatabaseInformation> Databases {
+        get => _Databases;
+        set {
+            _Databases = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Databases)));
+        }
+    }
+
+    private DatabaseInformation _SelectedDatabase;
+    public DatabaseInformation SelectedDatabase {
+        get => _SelectedDatabase;
+        set
+        {
+            _SelectedDatabase = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedDatabase)));
+        }
+    }
 }
