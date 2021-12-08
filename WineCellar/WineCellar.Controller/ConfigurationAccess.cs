378006e472eb38da60698c107a459eefd0aa15cb
Dennis Kroes <developer@dkroes.nl>
2021-12-08 08:57:51 +0100

    ConfigurationAccess for adding databases to the appsettings

---
+++ WineCellar/WineCellar.Controller/ConfigurationAccess.cs
@@ -0,0 +1,54 @@
+﻿using Model;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WineCellar.Model;
+
+namespace Controller;
+
+public static class ConfigurationAccess
+{
+    public static async void AddDatabase(DatabaseInformation information)
+    {
+        // Read the appsettings and convert it to a string
+        string json = await File.ReadAllTextAsync(GetAppsettingsPath());
+
+        // Load the appsettings.json as a JObject
+        JObject appsettings = JObject.Parse(json);
+        // Entry "Databases" is a keyvalue structure
+        JToken? databases = appsettings["Databases"];
+
+        // Add the object for databases into the json if it doesn't exist
+        if (databases is null)
+        {
+            appsettings["Databases"] = new JObject();
+            databases = appsettings["Databases"];
+        }
+
+        // Check if it exists and add an object to it
+        if (databases is not null && databases[information.Name] is not null)
+        {
+            JObject databaseObj = new();
+            databaseObj["Host"] = information.Host;
+            databaseObj["Port"] = information.Port;
+            databaseObj["User"] = information.User;
+            databaseObj["Password"] = information.Password;
+            databaseObj["Database"] = information.Database;
+
+            databases[information.Name] = databaseObj;
+        }
+
+        await File.WriteAllTextAsync(GetAppsettingsPath(), appsettings.ToString(Newtonsoft.Json.Formatting.Indented));
+    }
+
+    private static string GetAppsettingsPath()
+    {
+        bool useDevelopment = File.Exists(Path.Join(Directory.GetCurrentDirectory(), "appsettings.Development.json"));
+        return useDevelopment ?
+            Path.Join(Directory.GetCurrentDirectory(), "appsettings.Development.json")
+            : Path.Join(Directory.GetCurrentDirectory(), "appsettings.json");
+    }
+}
--- WineCellar/WineCellar.Model/DatabaseInformation.cs
+++ WineCellar/WineCellar.Model/DatabaseInformation.cs
@@ -8,12 +8,12 @@ namespace Model;
 
 public class DatabaseInformation
 {
-    public string? Name { get; set; }
-    public string? Host { get; set; }
-    public string? Port { get; set; }
-    public string? User { get; set; }
-    public string? Password { get; set; }
-    public string? Database { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Host { get; set; } = string.Empty;
+    public string Port { get; set; } = string.Empty;
+    public string User { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string Database { get; set; } = string.Empty;
 
     public string ConnectionString => $"Data Source={Host},{Port};Initial Catalog={Database};User ID={User};Password={Password};Connect Timeout=30";
 }
