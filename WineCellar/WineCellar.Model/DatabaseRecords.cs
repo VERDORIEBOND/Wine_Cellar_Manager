f94d3923ff06067affa9c41f11f92ba4fb2e818b
Dennis Kroes <developer@dkroes.nl>
2021-12-13 18:55:36 +0100

    Database Wine - Picture from VARCHAR to VARBINARY

--- WineCellar/WineCellar.Model/DatabaseRecords.cs
+++ WineCellar/WineCellar.Model/DatabaseRecords.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace WineCellar.Model;
 
-public record WineRecord(int Id, string Name, decimal Buy, decimal Sell, int TypeId, string Type, int CountryId, string Country, string Picture, int Year, int Content, decimal Alcohol, int Rating, string Description);
+public record WineRecord(int Id, string Name, decimal Buy, decimal Sell, int TypeId, string Type, int CountryId, string Country, byte[]? Picture, int Year, int Content, decimal Alcohol, int Rating, string Description);
 public record CountryRecord(int Id, string Name);
 public record TypeRecord(int Id, string Name);
 public record NoteRecord(int Id, string Name);
