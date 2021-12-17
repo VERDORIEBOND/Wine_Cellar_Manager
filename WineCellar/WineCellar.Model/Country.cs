1135ffbe8c86f349e4f8fc44537fd78cf99e1817
Dennis Kroes <developer@dkroes.nl>
2021-12-17 09:16:23 +0100

    Models for current record types

---
+++ WineCellar/WineCellar.Model/Country.cs
@@ -0,0 +1,18 @@
+﻿namespace Model;
+
+public class Country
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public Country() : this(0, string.Empty)
+    {
+
+    }
+
+    public Country(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
---
+++ WineCellar/WineCellar.Model/WineNote.cs
@@ -0,0 +1,18 @@
+﻿namespace WineCellar.Model;
+
+public class WineNote
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public WineNote() : this(0, string.Empty)
+    {
+
+    }
+
+    public WineNote(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
