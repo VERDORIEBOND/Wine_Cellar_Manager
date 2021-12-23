15fb82c045d727f625c7aa2a5dc8bdcac63f92cf
David Eertink <s1141915@student.windesheim.nl>
2021-12-23 14:38:24 +0100

    Click on location

--- WineCellar/WineCellar.GUI/GeographicView.xaml.cs
+++ WineCellar/WineCellar.GUI/GeographicView.xaml.cs
@@ -9,31 +9,53 @@ using Mapsui.Styles;
 using System.IO;
 using System.Reflection;
 using System.Windows.Controls;
+using Mapsui.UI.Wpf;
+using System.Diagnostics;
 
 namespace WineCellar;
 
 public partial class GeographicView : Window
 {
+    private struct Cords
+    {
+        public int id;
+        public double x, y;
+    }
+
+    private List<Feature> points = new List<Feature>();
+    private List<Cords> cords = new List<Cords>();
+    private bool wineClicked = false;
+
     public GeographicView()
     {
         InitializeComponent();
 
+        AddPoints();                        // AddPoints first!!
         MyMapControl.Map = CreateMap();
     }
 
-    public static Map CreateMap()
+    public Map CreateMap()
     {
         var map = new Map();
 
         map.Layers.Add(OpenStreetMap.CreateTileLayer());
-        map.Layers.Add(CreatePointLayer());
+        map.Layers.Add(CreatePointLayer(points));
 
         return map;
     }
 
-    private static MemoryLayer CreatePointLayer()
+    public void AddPoints()
     {
-        MemoryProvider memoryProvider = new MemoryProvider(CreatePoint());
+        // dummy data
+        points.Add(CreatePoint(6.079559, 52.500767, 3));
+        points.Add(CreatePoint(4.4210539, 44.0171384, 4));
+        points.Add(CreatePoint(0.4328022, 44.7959213, 5));
+        points.Add(CreatePoint(2.1774322, 41.3828939, 6));
+    }
+
+    private MemoryLayer CreatePointLayer(List<Feature> points)
+    {
+        MemoryProvider memoryProvider = new MemoryProvider(points);
 
         return new MemoryLayer
         {
@@ -44,13 +66,11 @@ public partial class GeographicView : Window
         };
     }
 
-    // Misschien iets met https://github.com/Mapsui/Mapsui/issues/136
-
-    private static SymbolStyle PointStyle()
+    private SymbolStyle PointStyle()
     {
         SymbolStyle pointStyle = new SymbolStyle()
         {
-            //BitmapId = GetBitmapIdForEmbeddedResource("Loading.CatenaMalbec.png"),
+            //BitmapId = GetBitmapIdForEmbeddedResource("pointer.jpg"),
             SymbolScale = 0.3,
             Fill = new Brush(Color.FromString("Red"))
         };
@@ -58,31 +78,60 @@ public partial class GeographicView : Window
         return pointStyle;
     }
 
-    private static int GetBitmapIdForEmbeddedResource(string imagePath)
+    private int GetBitmapIdForEmbeddedResource(string imagePath)
     {
         var assembly = typeof(GeographicView).GetTypeInfo().Assembly;
         var image = assembly.GetManifestResourceStream(imagePath);
         return BitmapRegistry.Instance.Register(image);
     }
 
-    private static List<Feature> CreatePoint()
+    private Feature CreatePoint(double longitude, double latitude, int id)
     {
-        List<Feature> points = new List<Feature>();
         var feature = new Feature();
-        var p1 = SphericalMercator.FromLonLat(6.079559, 52.500767);
+        var point = SphericalMercator.FromLonLat(longitude, latitude);
 
         feature.Styles.Add(PointStyle());
-        feature.Geometry = p1;
-        points.Add(feature);
+        feature.Geometry = point;
 
-        return points;
+        var pCords = new Cords();
+        pCords.id = id;
+        pCords.x = longitude; 
+        pCords.y = latitude;
+
+        cords.Add(pCords);
+
+        return feature;
     }
 
     void GeographicView_Closing(object sender, System.ComponentModel.CancelEventArgs e)
     {
-        MainWindow window = new MainWindow();
-        window.Show();
-        Application.Current.MainWindow = window;
+        if (!wineClicked)
+        {
+            MainWindow window = new MainWindow();
+            window.Show();
+            Application.Current.MainWindow = window;
+        }
     }
-    
+
+    private void MyMapControl_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        var screenPosition = e.GetPosition(MyMapControl);
+        var worldPosition = MyMapControl.Viewport.ScreenToWorld(screenPosition.X, screenPosition.Y);
+        var pointPosition = SphericalMercator.ToLonLat(worldPosition.X, worldPosition.Y);
+
+        //Debug.Print($"{lonLat.X}, {lonLat.Y}");
+        foreach (var item in cords)
+        {
+            //Debug.Print($"{item.x}, {item.y}");
+            if ((pointPosition.X > (item.x - 0.05) && pointPosition.X < (item.x + 0.05)) && (pointPosition.Y > (item.y - 0.05) && pointPosition.Y < (item.y + 0.05)))
+            {
+                wineClicked = true;
+
+                DetailedView detailedView = new DetailedView(item.id);
+                Application.Current.MainWindow = detailedView;
+                detailedView.Show();
+                Close();
+            }
+        }
+    }
 }
